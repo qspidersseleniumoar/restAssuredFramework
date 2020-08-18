2fc677e8a872e8f8d3b9adf9681867e27e6fe4ee
SR Studio <SR Studio@DESKTOP-930H2SV.lan>
2020-08-18 13:29:44 +0530

    add

--- RestAussuredMavenProject/src/test/java/com/rmgyantra/api/projecttest/CreateProject_dataProvider.java
+++ RestAussuredMavenProject/src/test/java/com/rmgyantra/api/projecttest/CreateProject_dataProvider.java
@@ -26,16 +26,13 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
 
-public class CreateProject extends BaseClass{
+public class CreateProject_dataProvider extends BaseClass{
 	
 	
 
-	@Test
-	public void createProjectTest() throws Throwable {
-		
-		System.out.println("test");
-	String actAPIProjectNAme = "ABC";
-	String projectStatus = "Completed";
+	@Test(dataProvider = "getData")
+	public void createProjectTest(String actAPIProjectNAme, String projectStatus) throws Throwable {
+	
         Project pObj = new Project(actAPIProjectNAme, "aug", "deepak", projectStatus, 12);
 		
    	 
@@ -58,6 +55,25 @@ public class CreateProject extends BaseClass{
        Assert.assertEquals(dbProjectNAme, actAPIProjectNAme);
        
 	
+	}
+	
+	@DataProvider
+	public Object[][] getData() {
+		Object[][] objArr = new Object[4][2];
+		objArr[0][0] = "IDBS-0";
+		objArr[0][1] = "Completed";
+		
+		objArr[1][0] = "IDBS-1";
+		objArr[1][1] = "Completed";
+		
+		objArr[2][0] = "IDBS-2";
+		objArr[2][1] = "Completed";
+		
+		objArr[3][0] = "IDBS-3";
+		objArr[3][1] = "Completed";
+		
+		return objArr;
+		
 	}
 
 }
